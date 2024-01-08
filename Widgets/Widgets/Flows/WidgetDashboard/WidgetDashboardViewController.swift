c332cdfccdfe5792055770345fefa64acfcd37b6
Keuahn L <klumanog.dev@gmail.com>
2024-01-08 08:47:17 +0400

    Implement Widget Dashboard in UI from ViewModel

--- Widgets/Widgets/Flows/WidgetDashboard/WidgetDashboardViewController.swift
+++ Widgets/Widgets/Flows/WidgetDashboard/WidgetDashboardViewController.swift
@@ -5,4 +5,83 @@
 //  Created by Keuahn Lumanog on 05/01/2024.
 //
 
-import Foundation
+import UIKit
+import Combine
+
+class WidgetDashboardViewController: BaseViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView?
+    
+    var viewModel: WidgetDashboardViewModel
+    var cancellables: Set<AnyCancellable> = []
+    
+    init(viewModel: WidgetDashboardViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        performInitialSetup()
+    }
+    
+    override func setupNavigationBar() {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+}
+
+// MARK: - Setup -
+
+extension WidgetDashboardViewController{
+    
+    private func performInitialSetup() {
+        viewModel.fetchDashboardData()
+        setupBinders()
+        setupCollectionView()
+    }
+    
+    private func setupBinders() {
+        viewModel.$dataSource.sink { [weak self] data in
+            guard let self = self, !data.isEmpty else {
+                return
+            }
+            self.collectionView?.setCollectionViewLayout(createLayout(), animated: true)
+            self.collectionView?.reloadData()
+        }.store(in: &cancellables)
+    }
+    
+    private func setupCollectionView() {
+        collectionView?.register(cell: WidgetCollectionViewCell.self)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+    }
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource -
+
+extension WidgetDashboardViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.getTotalWidgetItemsCount()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard indexPath.row < viewModel.getTotalWidgetItemsCount(),
+              let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WidgetCollectionViewCell.reusableIdentifier, for: indexPath) as? WidgetCollectionViewCell
+        else {
+            return UICollectionViewCell()
+        }
+     
+        if let model = viewModel.getItem(at: indexPath.row) {
+            cell.configure(model: model)
+        }
+        return cell
+    }
+    
+}
--- Widgets/Widgets/Flows/WidgetDashboard/WidgetDashboardViewModel.swift
+++ Widgets/Widgets/Flows/WidgetDashboard/WidgetDashboardViewModel.swift
@@ -6,3 +6,115 @@
 //
 
 import Foundation
+import Combine
+
+protocol WidgetDashboardViewModelProtocol {
+    func fetchDashboardData()
+    func getWidgetGroups() -> [WidgetGroup]
+    func getWidgetGroup(at index: Int) -> WidgetGroup?
+    func getWidgetItems(from group: WidgetGroup) -> [WidgetItem]
+    func getTotalWidgetGroupsCount() -> Int
+    func getTotalWidgetItemsCount() -> Int
+    func getItemsCount(at index: Int) -> Int
+    func getItem(at index: Int) -> WidgetItem?
+    func getMaxHeightOrWidthInGroup(from widgetGroup: WidgetGroup?, isMaxHeight: Bool) -> CGFloat
+    func getMaxHeightInGroup(at index: Int) -> CGFloat
+    func getTotalHeightOrWidthInGroup(from widgetGroup: WidgetGroup?, layoutType: WidgetLayoutType, isTotalHeight: Bool) -> CGFloat
+}
+
+class WidgetDashboardViewModel {
+    
+    @Published var dataSource: [WidgetGroup] = []
+    private var allItems: [WidgetItem] = []
+}
+
+extension WidgetDashboardViewModel: WidgetDashboardViewModelProtocol {
+        
+    func fetchDashboardData() {
+        dataSource = DashboardService().fetchDashboardData()
+        
+        dataSource.forEach {
+            allItems += $0.widgets
+        }
+    }
+    
+    // MARK: - Get Data -
+    
+    func getWidgetGroups() -> [WidgetGroup] {
+        return dataSource
+    }
+    
+    func getWidgetGroup(at index: Int) -> WidgetGroup? {
+        guard index <= dataSource.count else {
+            return nil
+        }
+        return dataSource[index]
+    }
+    
+    func getWidgetItems(from group: WidgetGroup) -> [WidgetItem] {
+        return group.widgets
+    }
+    
+    // MARK: - Items Count -
+    
+    func getTotalWidgetGroupsCount() -> Int {
+        return dataSource.count
+    }
+    
+    func getTotalWidgetItemsCount() -> Int {
+        var totalCount = 0
+        for group in dataSource {
+            totalCount += group.widgets.count
+        }
+        return totalCount
+    }
+    
+    func getItemsCount(at index: Int) -> Int {
+        guard let widgetGroup = getWidgetGroup(at: index), !widgetGroup.widgets.isEmpty else {
+            return 0
+        }
+        
+        return widgetGroup.widgets.count
+    }
+    
+    func getItem(at index: Int) -> WidgetItem? {
+        guard index <= allItems.count else {
+            return nil
+        }
+        
+        return allItems[index]
+    }
+    
+    // MARK: - Max Height -
+    
+    func getMaxHeightOrWidthInGroup(from widgetGroup: WidgetGroup?, isMaxHeight: Bool) -> CGFloat {
+        guard let widgetGroup = widgetGroup, !widgetGroup.widgets.isEmpty else {
+            return 0
+        }
+        
+        let allHeights = widgetGroup.widgets.compactMap { isMaxHeight ? $0.style?.size.height : $0.style?.size.width }
+        return allHeights.max() ?? 0
+    }
+    
+    func getMaxHeightInGroup(at index: Int) -> CGFloat {
+        guard let widgetGroup = getWidgetGroup(at: index), !widgetGroup.widgets.isEmpty else {
+            return 0
+        }
+        
+        let allHeights = widgetGroup.widgets.compactMap { $0.style?.size.height }
+        return allHeights.max() ?? 0
+    }
+    
+    func getTotalHeightOrWidthInGroup(from widgetGroup: WidgetGroup?, layoutType: WidgetLayoutType, isTotalHeight: Bool) -> CGFloat {
+        guard let widgetGroup = widgetGroup, !widgetGroup.widgets.isEmpty else {
+            return 0
+        }
+        
+        if layoutType == .horizontal, isTotalHeight {
+            return getMaxHeightOrWidthInGroup(from: widgetGroup, isMaxHeight: true)
+        }
+                                              
+        let allHeights = widgetGroup.widgets.compactMap { isTotalHeight ? $0.style?.size.height : $0.style?.size.width}
+        return allHeights.reduce(0, +)
+    }
+}
